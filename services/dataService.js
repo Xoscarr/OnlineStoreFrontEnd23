6dac23c58babac23fd0701f488aeb074e3fc47d7
Oscar Rodriguez <rrodriguezsame@gmail.com>
2021-11-14 11:47:33 -0800

    end of Class 2

---
+++ services/dataService.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    _id: "123product",
+    title: "Bananas",
+    catagory: "fruit",
+    image: "img1.jpg",
+    price: 12.99,
+    stock: 3,
+    minium: 1,
+  },
+  {},
+];
